48308c05d9338a84cd953a2fe2148c136c3cf36a
Sadık Çavdar <cavdar.sadik01@gmail.com>
2023-01-14 22:10:37 +0300

    project folder structure created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from "./Components/index"
 
 function App() {
   return (
     <div className="App">
+      <Todo/>
     </div>
   );
 }
---
+++ src/Components/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "./Header/index"
+import Main from "./Main/index"
+import Footer from "./Footer/index"
+const Todo = () => {
+  return (
+    <div>Todo
+        <Header/>
+        <Main/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Todo
