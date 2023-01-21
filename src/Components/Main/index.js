48c6478f9db0bb40384acac0210ce103c4dcc3c2
Sadık Çavdar <cavdar.sadik01@gmail.com>
2023-01-21 20:16:05 +0300

    main empty created and css responsive fixed

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -4,15 +4,23 @@ const Main = ({ todoList }) => {
   return (
 
     <section className='main'>
-      <ul>
-        {todoList.map((item, idx) => (
-          <li key={idx}>
-            <h2>{item.todoTitle}</h2>
-            <p>{item.todoContent}</p>
-            <button>X</button>
-          </li>
-        ))}
-      </ul>
+      {todoList.length > 0 ?
+        <ul>
+          {todoList.map((item, idx) => (
+            <li key={idx}>
+              <h2>{item.todoTitle}</h2>
+              <p>{item.todoContent}</p>
+              <button>X</button>
+            </li>
+          ))}
+        </ul>
+        :
+        <div className='main-empty'>
+          <hr />
+          <p className='no-tasks'>No Tasks</p>
+          <hr />
+        </div>
+      }
     </section>
 
   )
